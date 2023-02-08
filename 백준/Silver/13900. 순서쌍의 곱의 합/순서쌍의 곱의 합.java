8f90fb61fc338fa7edc09c083364a5c44c9f9fff
insu3160 <124024198+insu3160@users.noreply.github.com>
2023-02-08 19:51:20 +0900

    [Silver IV] Title: 순서쌍의 곱의 합, Time: 844 ms, Memory: 110348 KB -BaekjoonHub

--- 백준/Silver/13900. 순서쌍의 곱의 합/순서쌍의 곱의 합.java
+++ 백준/Silver/13900. 순서쌍의 곱의 합/순서쌍의 곱의 합.java
@@ -10,18 +10,21 @@ public class Main {
         }while (n > 100000 || n < 2);
 
         long ary[] = new long[n];
-
+        long arysum[] = new long[n];
         for (int i = 0; i < n; i++) {
             ary[i] = scanner.nextInt();
         }
-        long psum[] = new long[n];
-        psum[0] = ary[0];
+        
+        arysum[0] = ary[0];
+        
         for (int i = 1; i < n; i++) {
-            psum[i] = psum[i-1] + ary[i];
+            arysum[i] = arysum[i-1] + ary[i];
         }
+        
         long sum = 0;
+        
         for (int i = 0; i < n-1; i++) {
-            sum += ary[i]*(psum[n-1]-psum[i]);
+            sum += ary[i]*(arysum[n-1]-arysum[i]);
         }
 
 
