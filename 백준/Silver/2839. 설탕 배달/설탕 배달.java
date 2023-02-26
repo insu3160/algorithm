56cdcef69e55bb22e7025457ddc1f270c61961e5
insu3160 <124024198+insu3160@users.noreply.github.com>
2023-02-26 17:02:58 +0900

    [Silver IV] Title: 설탕 배달, Time: 132 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -2,27 +2,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
+
 public class Main {
+    static int cnt=0;
+    private static int Basket(int n) {
+        if (n<0){
+            return -1;
+        } else if (n==0) {
+            return cnt;
+        } else {
+            if (n % 5 == 0) {
+                cnt += n / 5;
+                return cnt;
+            } else {
+                cnt++;
+                return Basket(n - 3);
+            }
+        }
+
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int basket = 0;
-        int copyn = n;
-        while (copyn != 0){
-            if (copyn%10 == 0 || copyn %10 == 5){
-                copyn-=5;
-                basket++;
-            }
-            else {
-                copyn-=3;
-                basket++;
-            }
-            if (copyn<0){
-                basket = -1;
-                break;
-            }
-
-        }
-        System.out.println(basket);
+        int result = Basket(n);
+        System.out.println(result);
     }
+
+
+
 }
