a3acfc299296236d7dd5a43ffa0f393b53d0ea57
insu3160 <124024198+insu3160@users.noreply.github.com>
2023-12-31 13:07:06 +0900

    [level unrated] Title: 머쓱이보다 키 큰 사람, Time: 0.81 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array, int height) {
+        
+        return (int) Arrays.stream(array).filter(o -> o>height).count();
+    }
+}
