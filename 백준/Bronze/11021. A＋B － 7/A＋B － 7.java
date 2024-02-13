7f7017ff2f019ea4b9a6226832b9d4ba290bac7d
insu3160 <124024198+insu3160@users.noreply.github.com>
2024-02-13 17:00:40 +0900

    [Bronze V] Title: A+B - 7, Time: 160 ms, Memory: 16140 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int a = Integer.parseInt(br.readLine());
+        String[] str = new String[2];
+        int answer;
+        for (int i = 1; i <= a; i++) {
+            str = br.readLine().split(" ");
+            answer = Integer.parseInt(str[0])+Integer.parseInt(str[1]);
+            bw.write("Case #"+i+": "+answer+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
