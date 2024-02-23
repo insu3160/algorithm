270c519012537e727ff67ce6e67f9bf9430173ef
insu3160 <124024198+insu3160@users.noreply.github.com>
2024-02-23 15:11:48 +0900

    [Bronze III] Title: 최소, 최대, Time: 1864 ms, Memory: 318316 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] ary = new int[n];
+        for (int i = 0; i < n; i++) {
+            ary[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.stream(ary).min().getAsInt()+" "+ Arrays.stream(ary).max().getAsInt());
+    }
+}
